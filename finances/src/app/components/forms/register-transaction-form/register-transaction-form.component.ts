5040af2e640a2e7e495d5a4a1e745b312cbad444
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-16 16:44:45 -0300

    part 2

--- finances/src/app/components/forms/register-transaction-form/register-transaction-form.component.ts
+++ finances/src/app/components/forms/register-transaction-form/register-transaction-form.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { TransactionService } from '../../../services/transaction.service';
 
 @Component({
   selector: 'app-register-transaction-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-transaction-form.component.html',
   styleUrl: './register-transaction-form.component.scss'
 })
 export class RegisterTransactionFormComponent {
+  constructor(private transactionService: TransactionService) {}
+  value = new FormControl<string>("");
+  type = new FormControl<"entrada" | "saida">("entrada");
+  description = new FormControl<string>("");
 
+  onSubmit(event: Event){
+    event.preventDefault();
+    const data = {
+      value: this.value.value as string,
+      type: this.type.value as "entrada" | "saida",
+      description: this.description.value as string
+    }
+    this.transactionService.addTransaction(data);
+    this.value.setValue("");
+    this.type.setValue("entrada");
+    this.description.setValue("");
+  }
 }
